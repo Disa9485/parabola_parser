dcee464527c65602aacf8bb1c42ce758c59ed079
Collin Miller <cemiller1998@gmail.com>
2026-05-12 15:56:52 -0400

    Fix parsing, print max/min values

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <limits>
+#include <algorithm>
 
 int main() {
     // Open file
@@ -14,26 +16,64 @@ int main() {
         std::cout << "Opened \'" << filename << "\'";
     }
 
+    /*
+    Data structure:
+    x,y
+    -30.0000,1699.5677
+    -29.8999,1693.4813
+    -29.7998,1687.6761
+    -29.6997,1684.1360
+    -29.5997,1675.6774
+    -29.4996,1665.6081
+    -29.3995,1665.1472
+    */
+
     // Skip header
     std::string line;
     std::getline(file, line);
 
     // Read file into array
     std::vector<std::pair<float, float>> data; // <x, y>
-    while (std::getline(file, line, ',')) {
-        std::pair<float, float> point;                  // Create point
-        std::istringstream stringstream(line);          // Create stream from line
-        stringstream >> point.first >> point.second;    // Add x, y values to pair
-        data.push_back(point);                          // Add point to data structure
+    while (std::getline(file, line)) {
+        // Seperate comma from line
+        std::string x, y;
+        std::istringstream stringstream(line);
+        std::getline(stringstream, x, ',');
+        std::getline(stringstream, y, ',');
+        
+        // Add point to data structure
+        data.push_back({std::stof(x), std::stof(y)});         
+        
+        // std::cout << "X: " << x;
+        // std::cout << " Y: " << y << std::endl;
     }
 
     // Close file
     file.close();
 
-    // Check
+    // // Check data
+    // for (const auto &pair : data) {
+    //     std::cout << pair.first << ', ' << pair.second << std::endl;
+    // }
+
+    // Find min/max x/y values
+    float max_x = std::numeric_limits<float>::lowest();
+    float max_y = std::numeric_limits<float>::lowest();
+    float min_x = std::numeric_limits<float>::max();
+    float min_y = std::numeric_limits<float>::max();
     for (const auto &pair : data) {
-        std::cout << pair.first << ', ' << pair.second << std::endl;
+        if (pair.first > max_x) max_x = pair.first;
+        if (pair.first < min_x) min_x = pair.first;
+        if (pair.second > max_y) max_y = pair.second;
+        if (pair.second < min_y) min_y = pair.second;
     }
+
+
+    // Print results
+    std::cout << "Max X: " << max_x << std::endl;
+    std::cout << "Min X: " << min_x << std::endl;
+    std::cout << "Max Y: " << max_y << std::endl;
+    std::cout << "Min Y: " << min_y << std::endl;
 
     return 0;
 }
