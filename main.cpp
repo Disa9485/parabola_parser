9ddb748cd7785c8863bc8346322f05f10d634890
Collin Miller <cemiller1998@gmail.com>
2026-05-12 17:26:38 -0400

    Finish basic stats

--- main.cpp
+++ main.cpp
@@ -56,24 +56,80 @@ int main() {
     //     std::cout << pair.first << ', ' << pair.second << std::endl;
     // }
 
-    // Find min/max x/y values
+    // Find x/y statistics
     float max_x = std::numeric_limits<float>::lowest();
     float max_y = std::numeric_limits<float>::lowest();
     float min_x = std::numeric_limits<float>::max();
     float min_y = std::numeric_limits<float>::max();
+    
+    float sum_x = 0.0f, sum_y = 0.0f;
+    float sum_sq_x = 0.0f, sum_sq_y = 0.0f;
+    
+    std::vector<float> x_values, y_values;
+
     for (const auto &pair : data) {
-        if (pair.first > max_x) max_x = pair.first;
-        if (pair.first < min_x) min_x = pair.first;
-        if (pair.second > max_y) max_y = pair.second;
-        if (pair.second < min_y) min_y = pair.second;
+        float x = pair.first;
+        float y = pair.second;
+        
+        // 1. Determine min/max x/y values
+        if (x > max_x) max_x = x;
+        if (x < min_x) min_x = x;
+        if (y > max_y) max_y = y;
+        if (y < min_y) min_y = y;
+
+        // Sum x/y values
+        sum_x += x;
+        sum_y += y;
+
+        // Sum squared x/y values
+        sum_sq_x += x * x;
+        sum_sq_y += y * y;
+
+        x_values.push_back(x);
+        y_values.push_back(y);
     }
 
+    // 2. Get x/y mean
+    size_t n = data.size();
+    float mean_x = sum_x / n;
+    float mean_y = sum_y / n;
 
-    // Print results
+    // 3. Get x/y standard deviation
+    // sum_sq = sum(x^2)
+    // variance = (sum_sq / size(x)) - mean(x)^2
+    // standard deviation = sqrt(variance)
+    float sd_x = std::sqrt((sum_sq_x / n) - (mean_x * mean_x));
+    float sd_y = std::sqrt((sum_sq_y / n) - (mean_y * mean_y));
+
+    // 4. Get x/y median
+    float med_x, med_y;
+    std::sort(x_values.begin(), x_values.end());
+    std::sort(y_values.begin(), y_values.end());
+    
+    // If size of list is even, find mean between two middle indices
+    if(n % 2 == 0) {
+        med_x = (x_values[n / 2 - 1] + x_values[n / 2]) / 2.0f;
+        med_y = (y_values[n / 2 - 1] + y_values[n / 2]) / 2.0f;
+
+    // If size of list is odd, get middle index
+    } else {
+        med_x = x_values[n / 2];
+        med_y = y_values[n / 2];
+    }
+
+    // Print statistics results
     std::cout << "Max X: " << max_x << std::endl;
     std::cout << "Min X: " << min_x << std::endl;
+    std::cout << "Mean X: " << mean_x << std::endl;
+    std::cout << "Med X: " << med_x << std::endl;
+    std::cout << "SD X: " << sd_x << std::endl;
+    
     std::cout << "Max Y: " << max_y << std::endl;
     std::cout << "Min Y: " << min_y << std::endl;
+    std::cout << "Mean Y: " << mean_y << std::endl;
+    std::cout << "Med Y: " << med_y << std::endl;
+    std::cout << "SD Y: " << sd_y << std::endl;
+    
 
     return 0;
 }
